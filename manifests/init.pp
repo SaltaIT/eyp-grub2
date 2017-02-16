423190f130d50da821046963e8700e51638673d5
Jordi Prats <jprats@systemadmin.es>
2017-02-16 13:27:18 +0100

    canvi refresh per unless

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,6 @@ class grub2($transparent_huge_pages=undef) inherits grub2::params {
     exec { 'grub2 transparent huge pages':
       command => "sed 's/^GRUB_CMDLINE_LINUX=\"\\?\\([^\"]*\\)\"\\?/GRUB_CMDLINE_LINUX=\"\\1 transparent_hugepage=${transparent_huge_pages}\"/' -i /etc/default/grub",
       unless  => "grep 'transparent_hugepage=${transparent_huge_pages}' /etc/default/grub",
-      notify  => Exec['grub2-mkconfig'],
     }
 
     # fact
@@ -27,9 +26,9 @@ class grub2($transparent_huge_pages=undef) inherits grub2::params {
     }
 
     exec { 'grub2-mkconfig':
-      command     => "${grub2::params::grubmkconfig} -o ${bootcfg}",
-      refreshonly => true,
-      require     => Exec['grub2 transparent huge pages'],
+      command => "${grub2::params::grubmkconfig} -o ${bootcfg}",
+      unless  => "grep 'transparent_hugepage=${transparent_huge_pages}' ${bootcfg}",
+      require => Exec['grub2 transparent huge pages'],
     }
   }
 
